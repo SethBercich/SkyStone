3c5c634c80fa3db6081dfdedb6b7e0445773a2f8
ShakopeeRobotics <shakopeerobotics@gmail.com>
2020-10-12 16:15:51 -0500

    Added the arm code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 @TeleOp(name = "Test Program")
@@ -13,12 +14,17 @@ public class TeleOpTest extends LinearOpMode {
     private DcMotor motorBackLeft;
     private DcMotor motorBackRight;
 
+    private DcMotor armMotor;
+    private Servo armServo;
+
     @Override
     public void runOpMode() throws InterruptedException {
         motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft"); motorFrontLeft.setDirection(DcMotor.Direction.FORWARD);
         motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight"); motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
         motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft"); motorBackLeft.setDirection(DcMotor.Direction.FORWARD);
         motorBackRight = hardwareMap.dcMotor.get("motorBackRight"); motorBackRight.setDirection(DcMotor.Direction.REVERSE);
+        armMotor = hardwareMap.dcMotor.get("armMotor"); armMotor.setDirection(DcMotor.Direction.FORWARD);
+        armServo = hardwareMap.servo.get("armServo");
 
         waitForStart();
 
@@ -27,6 +33,16 @@ public class TeleOpTest extends LinearOpMode {
             motorBackLeft.setPower(gamepad1.left_stick_y - gamepad1.left_stick_x);
             motorFrontRight.setPower(gamepad1.right_stick_y - gamepad1.right_stick_x);
             motorBackRight.setPower(gamepad1.right_stick_y + gamepad1.right_stick_x);
+
+            //Arm Stuff
+            armMotor.setPower(gamepad2.left_stick_y);
+
+            if(gamepad2.a) {
+                armServo.setPosition(1);
+            } else if(gamepad2.b) {
+                armServo.setPosition(0);
+            }
+
             idle();
         }
     }
